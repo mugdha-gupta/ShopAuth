f2c5cac6ab529647ce3a96c0aec91d503491c9ae
nymishaj <nymz47@gmail.com>
2019-03-22 14:17:13 -0500

    added missing files

---
+++ admin-site/src/components/AddUserPage.js
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import axios from "axios";
+import {Form, Input, Radio, Tooltip, Icon, Button,} from 'antd';
+import "antd/dist/antd.css";
+
+const RadioGroup = Radio.Group;
+
+class RegistrationForm extends Component {
+  state = {
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        
+        const name = values.firstname + ' ' + values.lastname;
+        const email = values.email;
+        const cardid = values.cardid;
+        const adminlevel = values.adminlevel;
+        console.log('Received values of form: ', name, email, cardid, adminlevel);
+        axios
+        .post("http://localhost:8080/user", {name:name, email:email, admin_level:adminlevel, scanString:cardid})
+        .then(response => {
+          alert('success');
+          
+        })
+        .catch((error) => {
+          // Error
+          console.log(error);
+          alert('error could not post');
+
+        });
+
+      }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 10 },
+        sm: { span: 4 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 10 },
+      },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 16,
+          offset: 4,
+        },
+      },
+    };
+
+    return (
+      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+
+        <Form.Item
+          label="First Name"
+        >
+          {getFieldDecorator('firstname', {
+            rules: [{ required: true, message: 'Please enter a first name', whitespace: true }],
+          })(
+            <Input />
+          )}
+        </Form.Item>
+        <Form.Item
+          label="Last Name"
+        >
+          {getFieldDecorator('lastname', {
+            rules: [{ required: true, message: 'Please enter a last name', whitespace: true }],
+          })(
+            <Input />
+          )}
+        </Form.Item>
+        <Form.Item
+          label="E-mail"
+        >
+          {getFieldDecorator('email', {
+            rules: [{
+              type: 'email', message: 'Not a valid e-mail address',
+            }, {
+              required: true, message: 'Please enter an e-mail address',
+            }],
+          })(
+            <Input />
+          )}
+        </Form.Item>
+        
+      
+        <Form.Item
+          label={(
+            <span>
+              Comet Card ID&nbsp;
+              <Tooltip title="Swipe the Comet Card here">
+                <Icon type="question-circle-o" />
+              </Tooltip>
+            </span>
+          )}
+        >
+          {getFieldDecorator('cardid', {
+            rules: [{ required: true, message: 'Please enter a comet card ID', whitespace: true }],
+          })(
+            <Input />
+          )}
+        </Form.Item>
+        <Form.Item
+          label="Admin Level"
+        >
+          {getFieldDecorator('adminlevel', {rules: [{required: true, message: 'Please choose an admin level'}]})(
+            <RadioGroup name="radiogroup" initialValue={1}>
+              <Radio value={1}>Level 1</Radio>
+              <Radio value={2}>Level 2</Radio>
+              <Radio value={3}>Level 3</Radio>
+            </RadioGroup>
+          )}
+        </Form.Item>
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">Save</Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+const AddUserPage = Form.create({ name: 'register' })(RegistrationForm);
+
+export default AddUserPage;
