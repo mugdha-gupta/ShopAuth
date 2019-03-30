61927d496b82a231e72b2d5876f7204daa9c1171
nymishaj <nymz47@gmail.com>
2019-03-29 23:33:02 -0500

    redesigned status page

--- admin-site/src/Main.js
+++ admin-site/src/Main.js
@@ -4,9 +4,9 @@ import {
   NavLink,
   HashRouter
 } from "react-router-dom";
-import StatusPage from "./components/StatusPage"
-import UserPage from "./components/UserPage";
-import MachinePage from "./components/MachinePage";
+import StatusPage from "./components/StatusPage";
+import UserPage from "./components/user-page/UserPage";
+import MachinePage from "./components/machine-page/MachinePage";
 import "antd/dist/antd.css";
 
  
@@ -18,7 +18,7 @@ class Main extends Component {
         <div>
           <h1>Machine Shop Authorization</h1>
           <ul className="header">
-            <li><NavLink exact to="/">StatusPage</NavLink></li>
+            <li><NavLink exact to="/">Status</NavLink></li>
             <li><NavLink to="/users">Users</NavLink></li>
             <li><NavLink to="/machines">Machines</NavLink></li>
           </ul>
