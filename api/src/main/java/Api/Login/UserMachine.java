7ca53a6542e988ef386ed76a563f2e6876738ef8
Chris Janusa <chrisjanusa@yahoo.com>
2019-04-08 17:10:08 -0500

    Change API to handle time stamps

--- api/src/main/java/Api/Login/UserMachine.java
+++ api/src/main/java/Api/Login/UserMachine.java
@@ -3,8 +3,10 @@ package Api.Login;
 import Api.Machine.Machine;
 import Api.User.User;
 import io.swagger.annotations.ApiModelProperty;
+import org.springframework.lang.NonNull;
 
 import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
 
 public class UserMachine {
     @NotNull
@@ -15,9 +17,14 @@ public class UserMachine {
     @ApiModelProperty(notes = "The machine being used")
     private Machine machine;
 
+    @NotNull
+    @ApiModelProperty(notes = "The time the user started using the machine", dataType = "java.lang.String", example = "YYYY-MM-DDTHH:MI:SS")
+    private Timestamp start_time;
+
     public UserMachine(@NotNull User user, @NotNull Machine machine) {
         this.user = user;
         this.machine = machine;
+        this.start_time = new Timestamp(System.currentTimeMillis());
     }
 
     public UserMachine() {
@@ -37,6 +44,14 @@ public class UserMachine {
 
     public void setMachine(Machine machine) {
         this.machine = machine;
+    }
+
+    public Timestamp getStart_time() {
+        return start_time;
+    }
+
+    public void setStart_time(Timestamp start_time) {
+        this.start_time = start_time;
     }
 
     @Override
